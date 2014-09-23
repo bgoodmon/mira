c10a175c1c181cfedaa58dd7842b7eab8c1661ec
Alicia Cozine <alicia@curationexperts.com>
2014-09-23 15:28:11 -0500

    adds resque-web

--- config/routes.rb
+++ config/routes.rb
@@ -9,17 +9,20 @@ Tufts::Application.routes.draw do
   # This is from Blacklight::Routes#solr_document, but with the constraints added which allows periods in the id
   resources :solr_document, path: 'catalog', controller: 'catalog', only: [:show, :update]
   resources :downloads, only: [:show], constraints: { id: ALLOW_DOTS }
-
   resources :templates, only: [:index]
+
   unauthenticated do
     root to: 'contribute#redirect'
   end
+  
   root to: "catalog#index", as: :authenticated_root
+
   resources :unpublished, only: [:index] do
     member do
       get 'facet'
     end
   end
+  
   resources :deposit_types do
     get 'export', on: :collection
   end
@@ -29,7 +32,13 @@ Tufts::Application.routes.draw do
       get 'license'
     end
   end
+  
+  mount Hydra::RoleManagement::Engine => '/'
+  mount Resque::Server, => '/queues'
+  mount Qa::Engine => '/qa'
+
   mount HydraEditor::Engine => '/'
+
   post 'records/:id/publish', to: 'records#publish', as: 'publish_record', constraints: { id: ALLOW_DOTS }
   put 'records/:id/review', to: 'records#review', as: 'review_record', constraints: { id: ALLOW_DOTS }
   resources :records, only: [:destroy], constraints: { id: ALLOW_DOTS } do
@@ -44,11 +53,8 @@ Tufts::Application.routes.draw do
     get :new_xml_import, on: :collection
   end
 
-
-  mount Qa::Engine => '/qa'
-
   resources :generics, only: [:edit, :update], constraints: { id: ALLOW_DOTS }
 
   devise_for :users
-  mount Hydra::RoleManagement::Engine => '/'
+
 end
